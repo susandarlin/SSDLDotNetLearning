183ef896a394cad82a85bbdbac5ca26cba7053d1
Sandar <sandar@solidplm.com>
2025-04-25 00:04:28 +0700

    Add project files.

---
+++ SSDLDotNetLearning.ConsoleApp/HomeworkService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSDLDotNetLearning.ConsoleApp;
+
+internal class HomeworkService
+{
+    private readonly SqlConnectionStringBuilder _sqlConnectionStringBuilder = new SqlConnectionStringBuilder
+    {
+        DataSource = "SANDAR\\MSSQLSERVER2022",
+        InitialCatalog = "SSDLDotNetLearning",
+        UserID = "sa",
+        Password = "admin123!",
+        TrustServerCertificate = true
+    };
+
+    public void Read()
+    {
+        SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+        connection.Open();
+    }
+}
---
+++ SSDLDotNetLearning.ConsoleApp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SSDLDotNetLearning.ConsoleApp;
+
+Console.WriteLine("Hello, World!");
+
+HomeworkService service = new HomeworkService();
+service.Read();
