25d19370951be74bbc4dacbca4db6ab2f4bfdc97
Su Sandar Lin <susandarlin.2016@gmail.com>
2025-04-27 12:01:05 +0700

    feat: add Create, Update & Delete Options in program.cs

--- SSDLDotNetLearning.ConsoleApp2/Program.cs
+++ SSDLDotNetLearning.ConsoleApp2/Program.cs
@@ -5,7 +5,10 @@ BeforeSystem:
 Console.WriteLine("Inventory Management System");
 
 Console.WriteLine("1. View Products");
-Console.WriteLine("2. Exit");
+Console.WriteLine("2. Create Products");
+Console.WriteLine("3. Update Products");
+Console.WriteLine("4. Delete Products");
+Console.WriteLine("5. Exit");
 
 Console.Write("Select Option: ");
 int option = Convert.ToInt32(Console.ReadLine());
